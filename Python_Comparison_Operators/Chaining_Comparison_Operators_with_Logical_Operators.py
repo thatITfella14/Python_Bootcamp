bed14b67650cfa8c3b607f9732eff25941695b3b
Karan Patel <kanbij144@gmail.com>
2025-08-31 11:35:04 -0600

    What I learned on August_31_2025

---
+++ Python_Comparison_Operators/Chaining_Comparison_Operators_with_Logical_Operators.py
@@ -0,0 +1,19 @@
+# logical operators with comparison operators
+
+print(1 < 2 and 2 < 3)  # True
+
+print() # line break
+
+print("h" == "h" and 1 == 2)  # False
+
+print() # line break#
+
+print(1 < 2 or 2 > 3)  # True
+
+print() # line break
+
+print(not(1 == 1))  # False
+
+print() # line break
+
+print(not(1 < 2 and 2 < 3))  # False
---
+++ Python_Comparison_Operators/comparison_operators.py
@@ -0,0 +1,25 @@
+print(2 == 2)
+
+print() # line break
+
+print(2 == 1)
+
+print() # line break
+
+print("hello" == "bye")
+
+print() # line break
+
+print("hello" == "hello")
+
+print() # line break
+
+print(2.0 == 2)
+
+print() # line break
+
+print(3 != 3)
+
+print() # line break
+
+print(3 != 2)
