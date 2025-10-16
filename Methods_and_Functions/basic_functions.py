7c698b09693257268dc58f2d9039dfd6879f22bd
Karan Patel <kanbij144@gmail.com>
2025-10-15 20:51:35 -0600

    What I learned on October_15_2025

---
+++ Methods_and_Functions/basic_functions.py
@@ -0,0 +1,49 @@
+# Creating a function requires a very specific syntax, including the def keyword, correct identation, and proper structure.
+
+# Example 1
+def say_hello():
+    print("Hello!")
+
+say_hello()  # Calling the function to execute its code
+
+print()
+
+# Example 2
+
+def say_name(name):
+    print(f"Hello, {name}!")
+
+say_name("Kira")
+
+print()
+
+# Difference between print and return:
+
+# The core difference between print and return in Python is their purpose: print is for displaying output to a user, while return is for sending a value from a function back to the code that called it.
+
+def add_num(num1,num2):
+    return num1 + num2
+
+result = add_num(10,20)
+
+print(result) # output: 30
+
+print()
+
+# Another example:
+
+result = add_num(5,7)
+
+print(result) # Output: 12
+
+print()
+
+# Someone you can pass the print and return under the same function. It's not very common, but it can be done. Imagine a stiuation where you want to log the result of a calculation for debugging purposes, but also want to return the result for further use.
+
+def multiply_num(a,b):
+    print(a * b)  # this will print
+    return a * b
+
+result1 = multiply_num(2,2) # thid will store the return value in result1 and print the value as well when print is called upon it down below
+
+print(result1)
