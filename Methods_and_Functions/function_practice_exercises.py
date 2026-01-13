8484e55318625a7140f65918e719931c91e2495a
Karan Patel <kanbij144@gmail.com>
2026-01-12 20:08:09 -0700

    What I learned on January 12, 2026

--- Methods_and_Functions/function_practice_exercises.py
+++ Methods_and_Functions/function_practice_exercises.py
@@ -127,3 +127,27 @@ def spy_game(arr):
             if pointer == len(seq):
                 return True
     return False
+
+print()
+print()
+
+# COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
+
+def count_primes(num):
+    primes = [2]
+    if num < 2:
+        return 0
+    x = 3
+    while x <= num:
+        for y in range (3,x,2):
+            if x % y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+    print(primes)
+    return len(primes)
+
+print()
+print()
